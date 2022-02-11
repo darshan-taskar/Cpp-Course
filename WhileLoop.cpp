488313422125f3652273683b01cd4c5873bc3d00
darshan-taskar <programmingbeast22@gmail.com>
2022-02-11 19:31:26 +0530

    C++ program by using while loop

---
+++ WhileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    /*while (num1 > 0){
+        cout<<"Number is Positive"<<endl;
+        num1++;
+        break;
+       
+        
+    }*/
+
+    return 0;
+}
