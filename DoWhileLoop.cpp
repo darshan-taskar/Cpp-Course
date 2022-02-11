6c3f24f2edeb9698fc0f9e990d36322703eb4ff9
darshan-taskar <programmingbeast22@gmail.com>
2022-02-11 19:31:59 +0530

    C++ program by using Dowhile loop

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    cin >> num1;
+    do
+    {
+        num1++;
+        cout << num1 << endl;
+    } while (num1 >= 10);
+    return 0;
+}
